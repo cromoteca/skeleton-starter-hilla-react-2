e38995979e59686d9110250c8a37d30b91a8eec2
Luciano Vernaschi <luciano@vaadin.com>
2024-05-22 10:40:08 +0200

    Cleanup view

--- frontend/views/MainView.tsx
+++ frontend/views/MainView.tsx
@@ -1,14 +1,8 @@
 import { AutoGrid } from "@vaadin/hilla-react-crud";
-import { Button } from "@vaadin/react-components/Button.js";
-import { Notification } from "@vaadin/react-components/Notification.js";
-import { TextField } from "@vaadin/react-components/TextField.js";
-import { HelloEndpoint, ProductAdvancedDtoListService } from "Frontend/generated/endpoints.js";
+import { ProductAdvancedDtoListService } from "Frontend/generated/endpoints.js";
 import ProductAdvancedDtoModel from "Frontend/generated/org/vaadin/example/ProductAdvancedDtoModel";
-import { useState } from "react";
 
 export default function MainView() {
-  const [name, setName] = useState("");
-
   return (
     <>
       <AutoGrid model={ProductAdvancedDtoModel} service={ProductAdvancedDtoListService} hiddenColumns={['id', 'supplier.id']} />
